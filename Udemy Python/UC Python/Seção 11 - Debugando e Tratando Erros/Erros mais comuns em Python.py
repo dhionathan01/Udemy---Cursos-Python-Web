335a27c3d9faf6f8e7112c9deae1d59596cbcb17
Dhionathan Jobim <dhionathan01@hotmail.com>
2021-05-14 15:44:50 -0300

    UP => Material Curso Python

---
+++ Udemy Python/UC Python/Seção 11 - Debugando e Tratando Erros/Erros mais comuns em Python.py
@@ -0,0 +1,39 @@
+"""
+Erros mais comuns em Python
+
+ATENÇÃO! É importante prestar atenção e aprender a ler as saídas de erros geradas pela execução do nosso código.
+
+Os erros mais comuns:
+
+SyntaxError -> Ocorre quando o Python encontra um erro de sintaxe. Ou seja, você escreveu algo que o Python não
+reconhece como parte da linguagem
+"""
+
+# Exemplos SyntaxError
+# Exemplo 1:
+'''
+def funcao:
+    print('Dhionathan Jobim')
+'''
+"""
+Sabemos que uma função se dá pela abertura e fechamento de parêntese em sua definição, a forma correta seria
+def funcao():
+    print('Dhionathan Jobim')
+"""
+
+# Exemplo 2
+'''
+None = 1
+'''
+"""
+Sabemos que None é uma palavra reservada do sistema, logo ela não poder ser usada como variável e nem ter um
+valor atribuído a ela.
+"""
+
+# Exemplo 3
+'''
+return
+'''
+"""
+Sabemos que return é uma palavra reservada geralmente utilizada dentro de uma função que possui o return
+"""
