9afb45220d8c371073c0dafd1b0a3f7972947ceb
Dhionathan Jobim <dhionathan01@hotmail.com>
2021-06-08 15:43:57 -0300

    Up => Material Dev. Web(Bootstrap)

---
+++ workspace/exemplogiteclipse/src/exemplogiteclipse/TesteGit.java
@@ -0,0 +1,9 @@
+package exemplogiteclipse;
+
+public class TesteGit {
+
+	public static void main(String[] args) {
+		System.out.println("Teste de git");
+	}
+
+}
---
+++ workspace/exemplogiteclipse/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dhion
+ *
+ */
+module exemplogiteclipse {
+}
