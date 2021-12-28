a035fdc005c2b7ceebdba815c20e28c5c9662114
Dhionathan Jobim <dhionathan01@hotmail.com>
2021-12-28 16:06:26 -0300

    UP => Praticando com Exercícios em Python

---
+++ Udemy Python/UC Python/Seção 8 - Funções em Python/Exercícios/Exercicio_12.py
@@ -0,0 +1,20 @@
+"""
+Escreva uma função que receba um número inteiro maior do que zeo e retorne a soma de todos os seus algarismos.
+por exemplo ao número 251 corresponderá o valor 8 (2 + 5 +1).
+Se o número lido não for maior do que zero, o programa terminará com a mensagem "número inválido".
+"""
+
+
+def soma_algorismo(num):
+    num = list(num)
+    convert_to_int = []
+    for valor in num:
+        convert_to_int.append(int(valor))
+    return sum(convert_to_int)
+
+
+numero = input('Informe o Número: ')
+if int(numero) > 0:
+    print(f'Soma de cada caracter informado: {soma_algorismo(numero)}')
+else:
+    print('Número inválido')
