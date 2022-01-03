130e56aec533dfde3ede8790f44bf962609be789
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-01-03 15:47:56 -0300

    UP => Praticando com Exercícios em Python

---
+++ Udemy Python/UC Python/Seção 12 - Módulos e Pacotes em Python/pacotes.py
@@ -0,0 +1,12 @@
+"""
+Pacotes
+Módulo → É apenas um arquvi Python que pode ter diversas funções para utilizarmos.
+
+Pacote → é um diretório contendo uma coleção de módulos.
+
+OBS: Nas versões 2.x Python, um pacote Python deveria conter dentro dele
+um arquivo chamado __init__.py
+
+Nas versões do python 3.x, não é mais obrigatória a utilização deste arquivo, mas
+normalmente ainda é utilizado para manter compatibilidade
+"""
