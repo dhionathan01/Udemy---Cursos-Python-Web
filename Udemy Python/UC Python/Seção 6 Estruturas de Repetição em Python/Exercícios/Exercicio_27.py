3ed216fe96dbb418943bc90efdce988577138ea3
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-01-04 15:22:34 -0300

    UP => Python Praticando com Exercícios

---
+++ Udemy Python/UC Python/Seção 6 Estruturas de Repetição em Python/Exercícios/Exercicio_27.py
@@ -0,0 +1,12 @@
+"""
+Em Matemática, o número harmônico designado por H(n) define-se como sendo a soma da série harmônica:
+H(n) = 1 + 1/2 + 1/3 +1/4 + 1/5... 1/n
+Faça um programa que leia um valor n inteiro e positivo e apresente o valor de H(n)
+"""
+n = int(input('Informe o valor chave da série harmônica: '))
+contador = 1
+resultado = 0
+while contador < n:
+    resultado += 1/contador
+    contador += 1
+print(f'Valor de H({n}) equivale: {resultado}')
