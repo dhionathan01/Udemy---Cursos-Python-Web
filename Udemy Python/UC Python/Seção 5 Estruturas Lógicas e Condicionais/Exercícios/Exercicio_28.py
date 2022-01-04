a3f064a5dc3c3004633823755ada3a190e596d36
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-01-04 15:16:41 -0300

    UP => Fazendo Ajustes no Exercício 28

--- Udemy Python/UC Python/Seção 5 Estruturas Lógicas e Condicionais/Exercícios/Exercicio_28.py
+++ Udemy Python/UC Python/Seção 5 Estruturas Lógicas e Condicionais/Exercícios/Exercicio_28.py
@@ -14,26 +14,33 @@ calculo = input('Informe o calculo que deseja efetuar: \n'
                 'd - Aritmético \n'
                 ': ')
 calculo = calculo.lower()
+valores_validos = False
 x = int(input('Informe o valor x: '))
 y = int(input('Informe o valor y: '))
 z = int(input('Informe o valor z: '))
+if x > 0 and y > 0 and z > 0:
+    valores_validos = True
 total = 0
 tipo = ''
-if calculo == 'a':
-    tipo = 'Geométrica'
-    total = sqrt(x * y * z)
-elif calculo == 'b':
-    tipo = 'Ponderada'
-    total = (x + 2 * y + 3 * z) / 6
-elif calculo == 'c':
-    tipo = 'Hârmonica'
-    total = (1 / x + 1 / y + 1 / z) / 1
-elif calculo == 'd':
-    tipo = 'Aritmética'
-    total = (x + y + z) / 3
+
+if valores_validos:
+    if calculo == 'a':
+        tipo = 'Geométrica'
+        total = sqrt(x * y * z)
+    elif calculo == 'b':
+        tipo = 'Ponderada'
+        total = (x + 2 * y + 3 * z) / 6
+    elif calculo == 'c':
+        tipo = 'Hârmonica'
+        total = (1 / x + 1 / y + 1 / z) / 1
+    elif calculo == 'd':
+        tipo = 'Aritmética'
+        total = (x + y + z) / 3
+    else:
+        tipo = 'Opção escolhida não é válida'
+        total = False
 else:
-    tipo = 'Opção escolhida não é válida'
-    total = False
+    print('Um ou mais valores informados não é possitivo inteiro')
 
 print(f'Operação: {tipo} em que x = {x} y = {y} z = {z}\n'
       f'Equivale: {total:.2f}')
