3729bc01af45eea85009a6c721f4b365e85b6817
Dhionathan Jobim <dhionathan01@hotmail.com>
2022-01-09 19:53:02 -0300

    AppGame Mata Mosquito - Definindo Area Dinâmica

---
+++ Desenvolvimento Web Udemy/Seção 8 - Javascript/Projetos/AppGame Mata Mosquito/jogo.js
@@ -0,0 +1,9 @@
+var altura = 0
+var largura = 0
+function detectorDaAreaDisponivelDoJogo() {
+    var altura = window.innerHeight
+    var largura = window.innerWidth
+    console.log(altura, largura)
+}
+
+detectorDaAreaDisponivelDoJogo()
